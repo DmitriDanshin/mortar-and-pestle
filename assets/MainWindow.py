bf217e393e221ce8573302c6722678116a56fc41
Dmitri <Dmitri.Danshin2017@gmail.com>
2022-04-02 10:01:51 +0300

    Fixed a very dangerous bug!!!

--- assets/MainWindow.py
+++ assets/MainWindow.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '.\MainWindow.ui'
+# Form implementation generated from reading ui file 'templates\MainWindow.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.6
 #
@@ -24,18 +24,24 @@ class Ui_MainWindow(object):
         self.horizontalSlider_2 = QtWidgets.QSlider(self.centralwidget)
         self.horizontalSlider_2.setOrientation(QtCore.Qt.Horizontal)
         self.horizontalSlider_2.setObjectName("horizontalSlider_2")
-        self.gridLayout.addWidget(self.horizontalSlider_2, 1, 1, 1, 1)
+        self.gridLayout.addWidget(self.horizontalSlider_2, 1, 2, 1, 1)
         self.wavelength_label = QtWidgets.QLabel(self.centralwidget)
         self.wavelength_label.setMaximumSize(QtCore.QSize(100, 20))
         self.wavelength_label.setObjectName("wavelength_label")
-        self.gridLayout.addWidget(self.wavelength_label, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.wavelength_label, 0, 1, 1, 1)
         self.reflection_label = QtWidgets.QLabel(self.centralwidget)
         self.reflection_label.setObjectName("reflection_label")
-        self.gridLayout.addWidget(self.reflection_label, 1, 0, 1, 1)
+        self.gridLayout.addWidget(self.reflection_label, 1, 1, 1, 1)
         self.horizontalSlider = QtWidgets.QSlider(self.centralwidget)
         self.horizontalSlider.setOrientation(QtCore.Qt.Horizontal)
         self.horizontalSlider.setObjectName("horizontalSlider")
-        self.gridLayout.addWidget(self.horizontalSlider, 0, 1, 1, 1)
+        self.gridLayout.addWidget(self.horizontalSlider, 0, 2, 1, 1)
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setObjectName("label_2")
+        self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
         self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 1, 1)
         self.verticalLayout = QtWidgets.QVBoxLayout()
         self.verticalLayout.setObjectName("verticalLayout")
@@ -61,5 +67,7 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Mortal and pestle"))
-        self.wavelength_label.setText(_translate("MainWindow", "Длина волны"))
-        self.reflection_label.setText(_translate("MainWindow", "Коэффициент отражения зеркал"))
+        self.wavelength_label.setText(_translate("MainWindow", "__"))
+        self.reflection_label.setText(_translate("MainWindow", "__"))
+        self.label.setText(_translate("MainWindow", "Длина волны [нм]"))
+        self.label_2.setText(_translate("MainWindow", "Коэффициент отражение зеркал"))
