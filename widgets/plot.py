aa357493171f616a22ef04058eeacddebda23f1b
Dmitri <Dmitri.Danshin2017@gmail.com>
2022-04-12 20:03:21 +0300

    Refactoring

--- widgets/plot.py
+++ widgets/plot.py
@@ -1,67 +1,87 @@
 import math
+from dataclasses import dataclass
+
+import LightPipes as lp
 import matplotlib
 import numpy as np
-from LightPipes import nm, mm, Begin, Intensity
+from LightPipes import mm, nm
+from PyQt5.QtWidgets import QWidget
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg
-from PyQt5.QtWidgets import QWidget
+from matplotlib.figure import Figure
 
 matplotlib.use('Qt5Agg')
 
 
-class Plot(FigureCanvasQTAgg):
+@dataclass
+class Intensity:
+    wavelength: float
+    reflection: float
+    distance: float
+    medium: int
+    size: int
+    N: int
+    f: float
 
-    def TheExample(self, wavelength, reflection, distance, n):
-        wavelength = wavelength * nm  # Длина волны
-        size = 5 * mm  # Масштаб
-        N = 300  # Количество шагов интегрирования
-        f = 100 * mm
-        d = distance * mm
-        Dlabda = 0.0
-        nmedium = n
-        k = 2 * math.pi / wavelength
+    @property
+    def value(self) -> lp.Intensity:
+        return self.__get_value()
 
-        fig, ax1 = plt.subplots(2, figsize=(15, 15))
+    def __get_value(self) -> lp.Intensity:
+        k = 2 * math.pi / (self.wavelength * nm)
+        result = lp.Intensity(Fin=lp.Begin(self.size * mm, self.wavelength * nm, self.N))
+        step = self.size / self.N
 
-        k2 = 2 * math.pi / (wavelength + Dlabda)
-
-        F = Begin(size, wavelength, N)
-        I = Intensity(1, F)
-        step = size / N / mm
-        for i in range(1, N):
+        for i in range(1, self.N):
             xray = i * step
-            for j in range(1, N):
+            for j in range(1, self.N):
                 yray = j * step
-                X = xray * mm - size / 2
-                Y = yray * mm - size / 2
-                radius = math.hypot(X, Y)
-                theta = radius / f
-                delta2 = k * nmedium * d * math.cos(theta)
-                Inten = 0.5 / (1 + (4.0 * reflection / (1.0 - reflection)) * math.pow(math.sin(delta2), 2))
-                delta2 = k2 * nmedium * d * math.cos(theta)
-                I[i][j] = (Inten + 0.5 / (1 + (4.0 * reflection / (1.0 - reflection)) * math.pow(math.sin(delta2), 2)))
-        ax1[0].clear()
-        x1 = np.linspace(0, 1, N)
-        ax1[0].plot(x1, I[int(N / 2), :], "r")  # for 2d mode
+                x = (xray - self.size / 2)
+                y = (yray - self.size / 2)
+                radius = math.hypot(x, y)
+                theta = radius / self.f
+                delta = k * self.medium * self.distance * math.cos(theta) * mm
+                intensity = 0.5 / (1 + (4.0 * self.reflection / (1.0 - self.reflection)) * math.pow(math.sin(delta), 2))
+                result[i][j] = (intensity + 0.5 / (
+                        1 + (4.0 * self.reflection / (1.0 - self.reflection)) * math.pow(math.sin(delta), 2)))
+        return result
 
-        ax1[1].imshow(I, cmap="gist_heat")
-        ax1[1].axis('off')
-        ax1[1].axis('equal')
-        str = 'Распределение интенсивности'
-        ax1[0].set_title(str)
+
+class Plot(FigureCanvasQTAgg):
+    fig: Figure
+
+    @staticmethod
+    def get_figure(intensity: Intensity):
+        fig, axes = plt.subplots(2, figsize=(15, 15))
+        x = np.linspace(0, 1, intensity.N)
+        axes[0].clear()
+        axes[0].plot(x, intensity.value[int(intensity.N / 2), :], "r")  # for 2d mode
+        axes[1].imshow(intensity.value, cmap="gist_heat")
+        axes[1].axis('off')
+        axes[1].axis('equal')
+        axes[0].set_title('Распределение интенсивности')
         plt.close(fig)
         return fig
 
+    def calculate(self, wavelength, reflection, distance, medium):
+        intensity = Intensity(
+            wavelength=wavelength,
+            reflection=reflection,
+            distance=distance,
+            medium=medium,
+            N=300,
+            f=100,
+            size=5,
+        )
+        return self.get_figure(intensity)
+
     def __init__(self, wavelength, reflection, distance, n):
-        self.fig = None
-        self.axes = None
         self.init(wavelength, reflection, distance, n)
         super(Plot, self).__init__(self.fig)
 
     def init(self, wavelength, reflection, distance, n):
-        self.axes = None
-        self.fig = self.TheExample(wavelength, reflection, distance, n)
+        self.fig = self.calculate(wavelength, reflection, distance, n)
 
     def update_parameters(self, wavelength, reflection, distance, n):
         self.fig.clf()
